241639875a431c933f9f322ace5e2ea85bc7b19a
shoikeda60-cmd <shoikeda60@gmail.com>
2026-08-14 03:22:14 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,16 @@
-const CACHE='asset-goal-v1';
+const CACHE='asset-goal-v3-cloud-weekdays';
 const ASSETS=['./','./index.html','./manifest.webmanifest'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
+self.addEventListener('activate',e=>e.waitUntil(Promise.all([
+  caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))),
+  self.clients.claim()
+])));
+self.addEventListener('fetch',e=>{
+  if(e.request.mode==='navigate'){
+    e.respondWith(fetch(e.request).then(r=>{
+      const copy=r.clone(); caches.open(CACHE).then(c=>c.put('./index.html',copy)); return r;
+    }).catch(()=>caches.match('./index.html')));
+    return;
+  }
+  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
+});
