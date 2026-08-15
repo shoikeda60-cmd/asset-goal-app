544fc271b8589a6fc654454adf015280aaa3ab87
shoikeda60-cmd <shoikeda60@gmail.com>
2026-08-15 23:31:57 +0900

    Bump cache for tab label rename

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='asset-goal-v13-delete-sync';
+const CACHE='asset-goal-v14-tab-rename';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./cashflow.js','./delete-sync.js','./chart-axis.js','./rate-tab.js','./rate-sbi.js'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([
@@ -7,11 +7,11 @@ self.addEventListener('activate',e=>e.waitUntil(Promise.all([
 ])));
 function enhanceHtml(html){
   const scripts=[];
-  if(!html.includes('cashflow.js')) scripts.push('<script src="./cashflow.js?v=13"></script>');
-  if(!html.includes('delete-sync.js')) scripts.push('<script src="./delete-sync.js?v=13"></script>');
-  if(!html.includes('chart-axis.js')) scripts.push('<script src="./chart-axis.js?v=13"></script>');
-  if(!html.includes('rate-tab.js')) scripts.push('<script src="./rate-tab.js?v=13"></script>');
-  if(!html.includes('rate-sbi.js')) scripts.push('<script src="./rate-sbi.js?v=13"></script>');
+  if(!html.includes('cashflow.js')) scripts.push('<script src="./cashflow.js?v=14"></script>');
+  if(!html.includes('delete-sync.js')) scripts.push('<script src="./delete-sync.js?v=14"></script>');
+  if(!html.includes('chart-axis.js')) scripts.push('<script src="./chart-axis.js?v=14"></script>');
+  if(!html.includes('rate-tab.js')) scripts.push('<script src="./rate-tab.js?v=14"></script>');
+  if(!html.includes('rate-sbi.js')) scripts.push('<script src="./rate-sbi.js?v=14"></script>');
   return scripts.length?html.replace('</body>',scripts.join('')+'</body>'):html;
 }
 async function pageWithEnhancements(req){
